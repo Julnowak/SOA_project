6d493dd8a142691681dc21723235a3464074c285
Julnowak <julnowak@student.agh.edu.pl>
2024-06-04 12:45:00 +0200

    server down example

--- sr_app_front/src/admin/Negotiations.tsx
+++ sr_app_front/src/admin/Negotiations.tsx
@@ -16,6 +16,7 @@ interface Negotiation{
 const Negotiations = () => {
 const [username, setUsername] = useState('' as string| null);
 const [user_type, setUserType] = useState('' as string| null);
+const [serverFlag, setServerFlag] = useState(false);
 const [negotiations, setNegotiations] = useState([] as Negotiation[]);
 
 useEffect(
@@ -35,11 +36,13 @@ useEffect(
                       user_type,
                   })
                 });
-
+              setServerFlag(true);
               const data = await response.json();
                 // console.log(data)
               setNegotiations(data);
           } catch {
+
+                  setServerFlag(false);
                   console.log('dddd')
               }
           }
@@ -58,7 +61,9 @@ const goRouteId = (room) => {
       
   return (
     <div style={{margin: 20}}>
-        <h1 style={{textAlign: "center", margin: 40}}>Twoje negocjacje</h1>
+        { serverFlag?
+            <div>
+            <h1 style={{textAlign: "center", margin: 40}}>Twoje negocjacje</h1>
         <table  className="table table-striped table-sm">
         <thead>
           <tr style={{textAlign: "center"}}>
@@ -99,6 +104,20 @@ const goRouteId = (room) => {
         </tbody>
 
         </table>
+        </div>:
+            <div style={{ textAlign: "center", padding:60}}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="300" height="300" fill="currentColor"
+                     className="bi bi-exclamation-triangle" viewBox="0 0 16 16">
+                    <path
+                        d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.15.15 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.2.2 0 0 1-.054.06.1.1 0 0 1-.066.017H1.146a.1.1 0 0 1-.066-.017.2.2 0 0 1-.054-.06.18.18 0 0 1 .002-.183L7.884 2.073a.15.15 0 0 1 .054-.057m1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767z"/>
+                    <path
+                        d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
+                </svg>
+                <h1 style={{textAlign:"center", marginTop:20}}>Serwer nie działa :(</h1>
+            </div>
+        }
+
+
     </div>
   );
 };
