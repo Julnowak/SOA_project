945b2f90639fc914f7538f2d4e248cf7a700662e
SzewczykSzy <szewczyk.szy@gmail.com>
2024-06-14 23:36:44 +0200

    docker_path added

--- run_app.py
+++ run_app.py
@@ -23,7 +23,7 @@ def open_browser(browser, url):
         print(f"Unsupported browser: {browser}. Please use 'chrome', 'firefox', or 'edge'.")
 
 
-def main(browser):
+def main(args):
     # Define commands
     commands = [
         ("cd products_service && python manage.py runserver 8000", "Run products_service on port 8000"),
@@ -39,7 +39,7 @@ def main(browser):
         time.sleep(1)  # Slight delay to ensure terminals open sequentially
 
     # Start Docker Desktop
-    subprocess.Popen(["C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe"])  # Adjust path if necessary
+    subprocess.Popen([args.docker_path])  # Adjust path if necessary
 
     # Wait for Docker to start
     print("Waiting for Docker to start...")
@@ -57,11 +57,12 @@ def main(browser):
     time.sleep(5)
 
     # Open the browser in incognito mode
-    print(f"Opening {browser} in incognito mode...")
-    open_browser(browser, "http://localhost:3000")
+    print(f"Opening {args.browser} in incognito mode...")
+    open_browser(args.browser, "http://localhost:3000")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run application services and open the browser.')
     parser.add_argument('--browser', type=str, required=True, help='Browser to use (chrome, firefox, edge)')
+    parser.add_argument('--docker_path', type=str, required=False, default="C:\\Program Files\\Docker\\Docker\\Docker Desktop.exe", help='Path to Docker Desktop app (Docker Desktop.exe)')
     args = parser.parse_args()
-    main(args.browser)
+    main(args)
